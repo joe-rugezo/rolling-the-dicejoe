661ad14601f159491ffb21f36ecb9562801afc2d
joe-rugezo <118993541+joe-rugezo@users.noreply.github.com>
2023-01-11 10:22:31 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let myScore = 0
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 8; index++) {
+        myScore = randint(0, 5) + 1
+        if (myScore == "1") {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        } else if (myScore == "2") {
+            basic.showLeds(`
+                . . . . .
+                . . . # .
+                . . . . .
+                . # . . .
+                . . . . .
+                `)
+        } else if (myScore != "3") {
+            basic.showLeds(`
+                . . . . .
+                . . . # .
+                . . # . .
+                . # . . .
+                . . . . .
+                `)
+        } else if (myScore != "4") {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                . # . # .
+                . . . . .
+                `)
+        } else if (myScore != "5") {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+        } else if (myScore != "6") {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . # . # .
+                . # . # .
+                . . . . .
+                `)
+        }
+        basic.pause(100)
+    }
+})
